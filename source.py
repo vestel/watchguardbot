77d6197c2eeddd3708adae75ffc63bf542dd386c
Aleksandrs Zdancuks <aleksandrs.zdancuks@accenture.com>
2018-12-05 10:13:09 +0200

    Change of the Architecture

--- source.py
+++ source.py
@@ -6,7 +6,6 @@ import telepot
 from telepot.loop import MessageLoop
 from settings import TOKEN
 
-REGEXP = re.compile(r'.*?N?(\d{2}[\.|,|\s]\d{4,})N?[;|\s|,|\.]{1,10}E?(\d{2}[\.|,|\s]\d{4,})E?.*?')
 
 def waze_link(lat, lon):
     return "[Waze](https://www.waze.com/location?ll=%s,%s&navigate=yes)" % (lon, lat)
@@ -33,17 +32,50 @@ def prepare_response(num, coordinates):
     lat, lon = coordinates
     return 'Uwaga! #%d (%s, %s) %s %s %s %s %s' % (num, lat, lon, waze_link(lat,lon), gmaps_link(lat, lon), osmand_link(lat, lon), bing_link(lat,lon), bmaps_link(lat,lon)) 
 
+REGEXP = re.compile(r'.*?N?(\d{2}[\.|,|\s]\d{4,})N?[;|\s|,|\.]{1,10}E?(\d{2}[\.|,|\s]\d{4,})E?.*?')
+class CoordinatesParser(object):
+    def __init__(self, msg_text):
+        self.matches = REGEXP.findall(msg_text)
+
+    def valid(self):
+        return len(self.matches)>0
+
+    def normalize(self):
+        return self.matches
+
+
+class CoordinatesResponse(object):
+    def __init__(self, msg_text, msg_type):
+        self.valid =  msg_type == 'text'
+        parser = CoordinatesParser(msg_text)
+        self.valid = self.valid and parser.valid()
+        if self.valid:
+            self.coords = parser.normalize()
+
+    def responseMsg(self): 
+        text = ''
+        for idx, coordinates in enumerate(self.coords):
+            lat = sanitize_coordinate(coordinates[0])
+            lon = sanitize_coordinate(coordinates[1])
+            # TODO Flip coordinates during check in case of incorrect order
+            if math.trunc(float(lat)) in range(50, 61) and math.trunc(float(lon)) in range(18,29):
+                text += prepare_response(idx+1, (lat, lon))+'\n\n'
+        return text
+
+    def responseParams(self, reply):
+        return {'parse_mode': 'Markdown', 'disable_web_page_preview': True, 'reply_to_message_id': reply}
+
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
-    if content_type == 'text':
-        matches = REGEXP.findall(msg['text'])
-        for idx, coordinates in enumerate(matches):
-            lat = sanitize_coordinate(coordinates[0])
-            lon = sanitize_coordinate(coordinates[1])
-            if math.trunc(float(lat)) in range(50, 61) and math.trunc(float(lon)) in range(18,29):
-                bot.sendMessage(chat_id, prepare_response(idx+1, (lat, lon)), parse_mode='Markdown', disable_web_page_preview=True, reply_to_message_id=msg['message_id'])
+    cListener = CoordinatesResponse(msg['text'], content_type)
+    if cListener.valid:
+        bot.sendMessage(chat_id, cListener.responseMsg(), **cListener.responseParams(msg['message_id']))
+
+#    lListener = LursoftResponse(msg['text'])
+#    if lListener:
+#        bot.sendMessage(chat_id, lListener.responseToMsg(msg['text']), *lListener.responseParams())
 
 
 bot = telepot.Bot(TOKEN)
