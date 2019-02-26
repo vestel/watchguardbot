5bf0c0a7ef0fe8e506fd8ca1101f6de0e50703c4
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2019-02-26 23:30:41 +0200

    More or less stable version

---
+++ dabot/responders/simple_coordinates_responder.py
@@ -0,0 +1,51 @@
+import math
+import telepot 
+
+from parsers.simple_coordinates_parser import SimpleCoordinatesParser
+
+class SimpleCoordinatesResponder(object):
+    def __init__(self, msg):
+        msg_type, _chat_type, chat_id = telepot.glance(msg)
+        self.valid =  msg_type == 'text'
+        parser = SimpleCoordinatesParser(msg['text'])
+        self.valid = self.valid and parser.valid()
+        self.reply_to = msg['message_id']
+        if self.valid:
+            self.coords = parser.normalize()
+
+    def waze_link(self, lat, lon):
+        return "[Waze](https://www.waze.com/location?ll=%s,%s&navigate=yes)" % (lon, lat)
+
+    def gmaps_link(self, lat, lon):
+        return "[GMaps](https://www.google.com/maps/search/?api=1&query=%s,%s)" % (lat, lon)
+
+    def osmand_link(self, lat, lon):
+        return "[OSM](https://osmand.net/go?lat=%s&lon=%s&z=16)" % (lat, lon)
+
+    def bing_link(self, lat, lon):
+        return "[Bing](https://www.bing.com/maps/?v=2&cp=%s~%s&lvl=17&dir=0&sty=c&sp=point.%s_%s_Object)" % (lat, lon, lat, lon)
+
+    def bmaps_link(self, lat, lon):
+        return "[Baltic](https://balticmaps.eu/map/?lat=%s&lon=%s&color=red)"%(lat,lon)
+
+    def prepare_response(self, num, lat, lon):
+        return '%d* (%s, %s) %s %s %s %s %s' % (num, lat, lon, 
+            self.waze_link(lat,lon), self.gmaps_link(lat, lon), 
+            self.osmand_link(lat, lon), self.bing_link(lat,lon), 
+            self.bmaps_link(lat,lon)) 
+
+    def response_msg(self): 
+        text = '*#%s.' % self.reply_to
+        for idx, coordinates in enumerate(self.coords):
+            # TODO Flip coordinates during check in case of incorrect order
+            lat, lon = coordinates
+            if math.trunc(float(lat)) in range(50, 61) and math.trunc(float(lon)) in range(18,29):
+                text += self.prepare_response(idx+1, lat, lon)+'\n\n'
+            else:
+                lon, lat = coordinates
+                if math.trunc(float(lat)) in range(50, 61) and math.trunc(float(lon)) in range(18,29):
+                    text += self.prepare_response(idx+1, lat, lon)+'\n\n'
+        return text
+
+    def response_params(self):
+        return {'parse_mode': 'Markdown', 'disable_web_page_preview': True, 'reply_to_message_id': self.reply_to }
---
+++ dabot/source.py
@@ -0,0 +1,40 @@
+import math
+import sys
+import time
+import re
+import telepot
+from telepot.loop import MessageLoop
+
+from responders.simple_coordinates_responder import SimpleCoordinatesResponder
+from responders.simple_photo_url_fetcher import SimplePhotoUrlFetcher
+from settings import TOKEN, ALLOWED
+
+BOT = telepot.Bot(TOKEN)
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    # ACL check: only specific chats are allowed
+    if chat_id not in ALLOWED.keys():
+        print('Filtered: ', content_type, chat_type, chat_id)
+        return
+    else:
+        print('Received: ', content_type, ALLOWED[chat_id])
+        
+    # Responders are classes that respond to msg-s
+
+    cListener = SimpleCoordinatesResponder(msg)
+    if cListener.valid:
+        BOT.sendMessage(chat_id, cListener.response_msg(), **cListener.response_params())
+
+    cListener = SimplePhotoUrlFetcher(msg)
+    if cListener.valid:
+        BOT.sendPhoto(chat_id, cListener.response_msg(), **cListener.response_params())
+
+
+MessageLoop(BOT, handle).run_as_thread()
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(5)
