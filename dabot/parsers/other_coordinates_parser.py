8e5d1c764eb0e703d4bdaff8badb113bd9cfcf60
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2019-05-10 00:46:32 +0300

    More special symbols to handle

--- dabot/parsers/other_coordinates_parser.py
+++ dabot/parsers/other_coordinates_parser.py
@@ -1,11 +1,13 @@
 import re
 
-REGEXP = re.compile(r'.*?(\d{2}°\s?\d{2}\'\s?\d{2}")\s?[N|E]\s{1,5}(\d{2}°\s?\d{2}\'\s?\d{2}")\s?[N|E].*?')
-SUBRE = re.compile(r'(\d{2})°(\d{2})\'(\d{2})"')
+REGEXP = re.compile(r'.*?N?\s?(\d{1,3}°\s?\d{2}\'\s?\d{2}")\s?N?i[\s|,|\.|;]{1,5}E?\s?(\d{1,3}°\s?\d{2}\'\s?\d{2}")\s?E?.*?')
+SUBRE = re.compile(r'(\d{1,3})°(\d{2})\'(\d{2})"')
 class OtherCoordinatesParser(object):
     def __init__(self, msg_text):
-        msg_text = msg_text.replace('′','\'').replace('″','"')
+        msg_text = msg_text.replace('′','\'').replace('″','"').replace('”','"').replace('’','\'')
+        print('ÓCP:', msg_text)
         self.raw_matches = REGEXP.findall(msg_text)
+        print('ÓCP:', self.raw_matches)
 
     def valid(self):
         return len(self.raw_matches)>0
@@ -20,4 +22,5 @@ class OtherCoordinatesParser(object):
             degrees, minutes, seconds = SUBRE.findall(slon).pop()
             mlon = round(int(degrees) + (int(minutes)/60.0) + (int(seconds)/3600.0), 6)
             res.append((mlat, mlon))
+        print("OCP:", res)
         return res
